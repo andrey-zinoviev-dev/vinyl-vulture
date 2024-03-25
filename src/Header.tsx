be91def7e3f3f233f4da90c7b1c4ce875cbdfd10
KajjiMNK <kajji3@mail.ru>
2024-03-25 18:19:35 +0300

    site base + header

---
+++ src/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+export default function Header({}) {
+    
+  
+    return (
+      <header className="header">
+        <p>
+          VV
+        </p>
+        <div className='btn__div'>
+          <button className="h__btn">
+            Log in
+          </button>
+          <button className="h__btn">
+            Sign up
+          </button>
+        </div>
+      </header>
+    )
+  }

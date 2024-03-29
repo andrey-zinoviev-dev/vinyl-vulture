5afeb4b3b447c0730a9feb1324488868c9fead6f
KajjiMNK <kajji3@mail.ru>
2024-03-29 16:12:44 +0300

    +sections +search form

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,12 +6,35 @@ export default function Home({}) {
   
     return (
      <main className="main">
-        
-        <p>
-            Грувстрит
+        <section className='search'>
+            <h2>
+                Поиск по сайту
+            </h2>
 
-        </p>
+            <form className='search__form'>
+                <input className='search__input'>
+                </input>
+                <button className='h__btn'>
+                    Найти
+                </button>
+            </form>  
+        </section>
+        <section className='intro__cont'>
+            <div>
+                Контент
+            </div>
+            <ul>
+                <li>
 
+                </li>
+            </ul>
+        </section>
+        <section className='pop__cont'>
+            Популярные 
+        </section>
+        <section className='main__cont'>
+            Плитки с заголовком
+        </section>
      </main>
     )
   }

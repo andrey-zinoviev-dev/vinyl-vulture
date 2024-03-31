15c3484a1cfdd50e9824423b398665fa8acd69d7
Andrey Zinoviev <dronis_dronis@mail.ru>
2024-04-01 02:35:36 +0300

    updated main page layout

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './Home.css'
 import { popularAlbums } from './utils'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faArrowRight, faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faArrowRight, faMagnifyingGlass, faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { faInstagram, faTelegram, faVk } from '@fortawesome/free-brands-svg-icons'
 export default function Home() {
     const [popularOrder, setPopularOrder] = React.useState(0);
 
@@ -41,14 +42,33 @@ export default function Home() {
 
     return (
      <main className="main">
+        <ul className='social'>
+            <li>
+                <FontAwesomeIcon icon={faVk} />
+            </li>
+            <li>
+                <FontAwesomeIcon icon={faTelegram} />
+            </li>
+            <li>
+                <FontAwesomeIcon icon={faInstagram} />
+            </li>
+        </ul>
+        <section className='main__promo'>
+            <h1>VINYL VULTURE // VINYL VULTURE // VINYL VULTURE // VINYL VULTURE //</h1>
+        </section>
         <section className='search'>
-            <div>
-                <div className='search__text-wrapper'>
-                    <h2>
-                        Найти <span>любую</span> пластинку и стать коллекционером.
-                    </h2>
-                    {/* <p><span>V</span><span>V</span></p> */}
-                </div>
+            {/* <h2></h2> */}
+            <form className='search__form'>
+                <input placeholder='Найти любую пластинку' className='search__input'></input>
+                <button className='h__btn'>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </form> 
+            {/* <div>
+
+                <h2>
+                    Найти <span>любую</span> пластинку и стать коллекционером.
+                </h2>
                 <form className='search__form'>
                     <input className='search__input'>
                     </input>
@@ -56,8 +76,8 @@ export default function Home() {
                         Найти
                     </button>
                 </form> 
-            </div>
-            <div>
+            </div> */}
+            {/* <div>
                 <h2>
                     Быть в курсе - быть в теме, а значит - иметь ассортимент
                 </h2>
@@ -65,27 +85,35 @@ export default function Home() {
                     <input placeholder='почта'></input>
                     <button>Подписаться</button>
                 </form>
-            </div>
+            </div> */}
         </section>
         <section className='intro__cont'>
+            <h2><p>Vinyl</p> <p>Vulture</p> рекомендует</h2>
             <div className='intro__cont-popular'>
                 <div className='intro__cont-popular-navigation-wrapper'>
                     <img className='intro__cont-popular-cover' src={popularAlbums[popularOrder].cover} />
                     <div className='intro__cont-popular-navigation-wrapper-buttons'>
-                        <button>
+                        <button onClick={() => {
+                            setPopularOrder((prevValue) => {
+                                return prevValue === 0 ? 0 : prevValue - 1;
+                            })
+                        }}>
                             <FontAwesomeIcon icon={faArrowLeft} />
                         </button>
-                        <button>
+                        <button onClick={() => {
+                            setPopularOrder((prevValue) => {
+                                return prevValue === popularAlbums.length -1 ? 0 : prevValue + 1;
+                            })
+                        }}>
                             <FontAwesomeIcon icon={faArrowRight} />
                         </button>
                     </div>
                 </div>
                 <div className='intro__cont-popular-albums-wrapper'>
-                    <h3>Vinyl Vulture рекомендует</h3>
                     <h2>0{popularOrder + 1} - {popularAlbums[popularOrder].name}</h2>
                     <ul className='intro__cont-popular-albums-wrapper-ul'>
                         {popularAlbums[popularOrder].songs.map((song, index) => {
-                            return <li key={song.name+index} style={{color: popularOrder === index ? "black" : "#B4B4B4", backgroundColor: popularOrder === index ? "#FE9E01" : "transparent"}}>
+                            return <li key={song.name+index} /*style={{color: popularOrder === index ? "black" : "#B4B4B4", backgroundColor: popularOrder === index ? "#FE9E01" : "transparent"}}*/ >
                                 <span>0{index + 1}</span>
                                 <h4>{song.name}</h4>
                                 <span style={{margin: "0 0 0 auto"}}>{song.length}</span>
